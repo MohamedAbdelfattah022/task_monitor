2dad6ad537e35ce72b6a659d801bb362be6cdfc8
MohamedAbdelfattah022 <mhabdelfattah22@gmail.com>
2023-12-24 18:11:54 +0200

    seperation of the task screen and task card

---
+++ lib/TaskCard.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'classes.dart';
+
+class TaskCard extends StatelessWidget {
+  final Task task;
+
+  TaskCard({required this.task});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.only(bottom: 16),
+      padding: EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border.all(color: Colors.grey),
+        borderRadius: BorderRadius.circular(8),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.shade300,
+            offset: Offset(0, 2),
+            blurRadius: 6,
+          ),
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            task.getName(),
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 10),
+          Text(
+            'Priority: ${task.getPriority().join(", ")}',
+            style: TextStyle(color: Colors.black),
+          ),
+          Text(
+            'Assigner: ${task.getAssignerName()}',
+            style: TextStyle(color: Colors.black),
+          ),
+          Text(
+            'Progress Level: ${task.getProgressLevel().join(", ")}',
+            style: TextStyle(color: Colors.black),
+          ),
+          Text(
+            'Start Time: ${task.getStartTime()}',
+            style: TextStyle(color: Colors.black),
+          ),
+          Text(
+            'Deadline: ${task.getDeadline()}',
+            style: TextStyle(color: Colors.black),
+          ),
+          Text(
+            'Description: ${task.getDescription()}',
+            style: TextStyle(color: Colors.black),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/TaskScreen.dart
+++ lib/TaskScreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'classes.dart';
+import 'TaskCard.dart';
 
 class TasksScreen extends StatelessWidget {
   final Project project;
@@ -19,71 +20,9 @@ class TasksScreen extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             SizedBox(height: 20),
-            // Display the list of tasks
             for (Task task in project.getTasks()) TaskCard(task: task),
           ],
         ),
-      ),
-    );
-  }
-}
-
-class TaskCard extends StatelessWidget {
-  final Task task;
-
-  TaskCard({required this.task});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      margin: EdgeInsets.only(bottom: 16),
-      padding: EdgeInsets.all(16),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        border: Border.all(color: Colors.grey),
-        borderRadius: BorderRadius.circular(8),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.grey.shade300,
-            offset: Offset(0, 2),
-            blurRadius: 6,
-          ),
-        ],
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            task.getName(),
-            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-          ),
-          SizedBox(height: 10),
-          Text(
-            'Priority: ${task.getPriority().join(", ")}',
-            style: TextStyle(color: Colors.black),
-          ),
-          Text(
-            'Assigner: ${task.getAssignerName()}',
-            style: TextStyle(color: Colors.black),
-          ),
-          Text(
-            'Progress Level: ${task.getProgressLevel().join(", ")}',
-            style: TextStyle(color: Colors.black),
-          ),
-          Text(
-            'Start Time: ${task.getStartTime()}',
-            style: TextStyle(color: Colors.black),
-          ),
-          Text(
-            'Deadline: ${task.getDeadline()}',
-            style: TextStyle(color: Colors.black),
-          ),
-          Text(
-            'Description: ${task.getDescription()}',
-            style: TextStyle(color: Colors.black),
-          ),
-        ],
       ),
     );
   }
